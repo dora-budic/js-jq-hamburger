e1fdc4cde7d50b792dd2b4795f1cc23355f3d0d6
Dora Budić <dbudic0706@gmail.com>
2021-03-10 16:13:02 +0100

    Added solution using add/removeClass

--- main.js
+++ main.js
@@ -1,20 +1,43 @@
 // Prendo l'icona del menu/hamburger
 var menu = $('.fas.fa-bars');
 
-// Prendo la lista del menu
+// // Prendo la lista del menu
 var list = $('.hamburger-menu');
 
 // Click al Hamburger
 menu.click(function () {
   // Faccio aparire la lista
-  list.show();
+  list.addClass("active");
 });
 
-// Prendo x per chiudere il menu
+// // Prendo x per chiudere il menu
 var close = $('.fas.fa-times');
 
 // Click al icona x
 close.click(function () {
   // Faccio scomparire la lista
-  list.hide();
+  list.removeClass("active");
 });
+
+
+// ALTERNATIVA
+// // Prendo l'icona del menu/hamburger
+// var menu = $('.fas.fa-bars');
+//
+// // Prendo la lista del menu
+// var list = $('.hamburger-menu');
+//
+// // Click al Hamburger
+// menu.click(function () {
+//   // Faccio aparire la lista
+//   list.show();
+// });
+//
+// // Prendo x per chiudere il menu
+// var close = $('.fas.fa-times');
+//
+// // Click al icona x
+// close.click(function () {
+//   // Faccio scomparire la lista
+//   list.hide();
+// });
